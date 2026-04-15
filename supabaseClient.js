e459db59287f27fc4189317cf51ca75e7e1a9405
Igorse <igorsena553@gmail.com>
2026-04-15 17:29:15 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const SUPABASE_URL = "COLE_AQUI_SUA_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "COLE_AQUI_SUA_SUPABASE_ANON_KEY";
+const SUPABASE_URL = https://wknxkyarvirxlerzqhor.supabase.co;
+const SUPABASE_ANON_KEY = sb_publishable_VCRd74KVBRnqQt9PBMnTOA_xvXKTkPy;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
